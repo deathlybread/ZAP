0c0976f753a16c2b40162fa5ba926336cd4e5f45
deathlybread <prithvi@prithvikohli.co.uk>
2014-11-29 22:01:36 +0000

    Commit 3

--- game.js
+++ game.js
@@ -10,16 +10,67 @@ window.onload = function () {
     
     //Call main loop function to initialise game
     mainLoop();
-    
-    function mainLoop () {
-        //Title scree
-        var logo = new Image();
-        
-        //Draw logo onto canvas
-        logo.onload = function () {
-            ctx.drawImage(logo, 150, 25);
-        };
-        
-        logo.src = 'images/ZAP-Logo2.png';
-    }
 };
+
+function mainLoop () {
+    //Show title screen
+    splashScreen();
+}
+function splashScreen () {
+    var logo = new Image();
+        
+    //Draw logo onto canvas
+    logo.onload = function () {
+        ctx.drawImage(logo, 150, 25);
+    };
+        
+    logo.src = 'images/ZAP-Logo2.png';
+        
+    //Menu selected option number (Get default value initially)
+    var optionSelect = 0;
+    //Draw menu options taking into account selected option
+    setInterval(function () {
+    if (optionSelect == 0) {
+        ctx.fillStyle = '#e74c3c'  
+    }
+    else {
+        ctx.fillStyle = 'white';
+    }
+    ctx.font = '40px PixelDart';
+    ctx.fillText('Play', 210, 255);
+    
+    if (optionSelect == 1) {
+        ctx.fillStyle = '#e74c3c'  
+    }
+    else {
+        ctx.fillStyle = 'white';
+    }
+    ctx.font = '40px PixelDart';
+    ctx.fillText('Options', 210, 305);
+    
+    if (optionSelect == 2) {
+        ctx.fillStyle = '#e74c3c'  
+    }
+    else {
+        ctx.fillStyle = 'white';
+    }
+    ctx.font = '40px PixelDart';
+    ctx.fillText('Credits', 210, 355);
+    }, 1);
+    
+    setInterval(function () {
+        document.onkeydown = function (e) { 
+
+        if (e.keyCode == '40' && optionSelect < 2) {
+            optionSelect++;
+            document.getElementById('menu-select').play();
+        }
+        if (e.keyCode == '38' && optionSelect >= 1) {
+            optionSelect--;
+            document.getElementById('menu-select').play();
+        }
+    };   
+    }, 1);
+}
+
+
